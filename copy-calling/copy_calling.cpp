a4022c515e9a7d84d567d66e71cac8e7c88b6c24
Jayden <cosbyemails@gmail.com>
2021-11-11 15:16:25 -0500

    Fix for multiple return instances, 0xC2 returns and 0xC3, and proper function end.

--- copy-calling/copy_calling.cpp
+++ copy-calling/copy_calling.cpp
@@ -1,113 +1,122 @@
 #include "copy_calling.h"
 
-void* copy_calling::create::create_documented_copy(LPCSTR module, LPCSTR function)
+#include <array>
+#include <map>
+
+// [byte, size (not including opcode)]
+static const std::map<std::uint8_t, std::uint8_t> return_bytes{
+	{ 0xC3, 0 },
+	{ 0xC2, 2 },
+};
+
+// can use std::to_array in C++20, otherwise declare a user-defined literal and specify no template params.
+static const std::array<std::uint8_t, 2> alignment_bytes{
+	0xCC,
+	0x90
+};
+
+void* copy_calling::create::create_documented_copy(const LPCSTR module, const LPCSTR function)
 {
-    HMODULE h_module = GetModuleHandleA(module);
-    std::vector<BYTE> v_bytes;
+	const auto handle = GetModuleHandleA(module);
 
-    if (h_module != INVALID_HANDLE_VALUE)
-    {
-        void* address = GetProcAddress(h_module, function);
-
-        if (!address)
-            return nullptr;
-
-        for (int i = 0; true; i++)
-        {
-            void* b_address = (void*)((uintptr_t)address + (uintptr_t)(i * sizeof(BYTE)));
-
-            v_bytes.push_back(*(BYTE*)b_address);
-
-            if (*(BYTE*)b_address == 0xC3)
-                break;
-        }
-
-		void* alloc = copy_calling::utility::allocate_memory_close_to_address(address, v_bytes.size());
-
-        if (alloc)
-        {
-            for (int i = 0; i < v_bytes.size(); i++)
-            {
-                memcpy((void*)((uintptr_t)alloc + (uintptr_t)(i * sizeof(BYTE))), (void*)&v_bytes[i], sizeof(BYTE));
-            }
-
-            return alloc;
-        }
-    }
-
-    return nullptr;
+	// not doing handle standalone here may throw a warning
+	return handle && handle != INVALID_HANDLE_VALUE ? create_undocumented_copy(GetProcAddress(handle, function)) : nullptr;
 }
 
 void* copy_calling::create::create_undocumented_copy(void* address)
 {
-    if (!address)
-        return nullptr;
+	if (!address)
+		return nullptr;
 
-    std::vector<BYTE> v_bytes;
+	std::vector<BYTE> v_bytes;
 
-    for (int i = 0; true; i++)
-    {
-        void* b_address = (void*)((uintptr_t)address + (uintptr_t)(i * sizeof(BYTE)));
+	for (std::uintptr_t i = 0u;; i++)
+	{
+		const auto b_address = reinterpret_cast<BYTE*>(reinterpret_cast<uintptr_t>(address) + (i * sizeof(BYTE)));
+		auto kill = false;
 
-        v_bytes.push_back(*(BYTE*)b_address);
+		v_bytes.push_back(*b_address);
 
-        if (*(BYTE*)b_address == 0xC3)
-            break;
-    }
+		// in C++17 you can use structured bindings here, since the project is C++14 I'll stick to referencing the pair elements
+		for (const auto& elem : return_bytes)
+		{
+			const auto& opcode = elem.first;
+			const auto& size = elem.second;
 
-    void* alloc = copy_calling::utility::allocate_memory_close_to_address(address, v_bytes.size());
 
-    if (alloc)
-    {
-        for (int i = 0; i < v_bytes.size(); i++)
-        {
-            memcpy((void*)((uintptr_t)alloc + (uintptr_t)(i * sizeof(BYTE))), (void*)&v_bytes[i], sizeof(BYTE));
-        }
+			if (*(b_address - size) == opcode)
+			{
+				// checks if at total end of function
+				// not going to reference 1 byte
+				for (const auto alignment : alignment_bytes)
+					if (*(b_address + 1) == alignment)
+					{
+						kill = true;
+						break;
+					}
 
-        return alloc;
-    }
+				// exit parent loop
+				if (kill)
+					break;
+			}
+		}
 
-    return nullptr;
+		if (kill)
+			break;
+	}
+
+	const auto alloc = copy_calling::utility::allocate_memory_close_to_address(address, v_bytes.size());
+
+	if (alloc)
+	{
+		std::memcpy(alloc, v_bytes.data(), v_bytes.size());
+
+		return alloc;
+	}
+
+	return nullptr;
 }
 
-void* copy_calling::utility::allocate_memory_close_to_address(void* address, size_t size)
+void* copy_calling::utility::allocate_memory_close_to_address(void* address, const std::size_t size)
 {
-	SYSTEM_INFO system_info; GetSystemInfo(&system_info);
-	const uintptr_t page_size = system_info.dwPageSize;
+	SYSTEM_INFO system_info{};
+	GetSystemInfo(&system_info);
 
-	uintptr_t start_adress = (uintptr_t(address) & ~(page_size - 1));
-	uintptr_t min = min(start_adress - 0x7FFFFF00, (uintptr_t)system_info.lpMinimumApplicationAddress);
-	uintptr_t max = max(start_adress + 0x7FFFFF00, (uintptr_t)system_info.lpMaximumApplicationAddress);
+	const auto page_size = static_cast<std::uintptr_t>(system_info.dwPageSize);
 
-	uintptr_t start_page = (start_adress - (start_adress % page_size));
+	const auto start_adress = (reinterpret_cast<std::uintptr_t>(address) & ~(page_size - 1));
+	const auto min = min(start_adress - 0x7FFFFF00, reinterpret_cast<std::uintptr_t>(system_info.lpMinimumApplicationAddress));
+	const auto max = max(start_adress + 0x7FFFFF00, reinterpret_cast<std::uintptr_t>(system_info.lpMaximumApplicationAddress));
 
-	uintptr_t page = 1;
+	const auto start_page = (start_adress - (start_adress % page_size));
+
+	std::size_t page = 1;
 	while (true)
 	{
-		uintptr_t byte_offset = page * page_size;
-		uintptr_t high = start_page + byte_offset;
-		uintptr_t low = (start_page > byte_offset) ? start_page - byte_offset : 0;
+		const auto byte_offset = page * page_size;
+		const auto high = start_page + byte_offset;
+		const auto low = (start_page > byte_offset) ? start_page - byte_offset : 0;
 
-		bool stop_point = high > max && low < min;
+		const auto stop_point = high > max && low < min;
 
 		if (!low)
 			continue;
 
 		if (high < max)
 		{
-			void* outAddr = VirtualAlloc((void*)high, size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-			if (outAddr)
-				return outAddr;
+			auto const out_addr = VirtualAlloc(reinterpret_cast<void*>(high), size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+			if (out_addr)
+				return out_addr;
 		}
 
 		if (low > min)
 		{
-			void* outAddr = VirtualAlloc((void*)low, size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-			if (outAddr)
-				return outAddr;
+			auto const out_addr = VirtualAlloc(reinterpret_cast<void*>(low), size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+			if (out_addr)
+				return out_addr;
 		}
 
-		page += 1;
+		page++;
 
 		if (stop_point)
 			break;
